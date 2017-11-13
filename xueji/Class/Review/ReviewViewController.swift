b2bba9969385bca6799d5afba70c9cea15384378
lvxin <18730231789@163.com>
2017-11-13 15:05:57 +0800

    复习 头部

--- xueji/Class/Review/ReviewViewController.swift
+++ xueji/Class/Review/ReviewViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class ReviewViewController: BaseViewController {
-
+    
+    /// 头部视图
+    var headBackView : UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "复习"
         // Do any additional setup after loading the view.
+        self.navigation_title_fontsize(name: "复习", fontsize: 20)
+        self.navigationBar_rightBtn_image(image: #imageLiteral(resourceName: "review_more"))
+        self.creatHeadView()
+        
+    }
+    // MARK: -头部视图
+    func creatHeadView() {
+        headBackView = UIView(frame: CGRect(x: 0, y: LNAVIGATION_HEIGHT, width: KSCREEN_WIDTH, height: ip6(46)))
+        self.view.addSubview(headBackView)
+        
+        let topLine : UIView = UIView(frame: CGRect(x: 0, y: 0, width: KSCREEN_WIDTH, height: ip6(2)))
+        topLine.backgroundColor = black_22
+        headBackView.addSubview(topLine)
+        
+        let bottomLine : UIView = UIView(frame: CGRect(x: 0, y: ip6(42), width: KSCREEN_WIDTH, height: ip6(2)))
+        bottomLine.backgroundColor = black_22
+        headBackView.addSubview(bottomLine)
     }
 
     override func didReceiveMemoryWarning() {
