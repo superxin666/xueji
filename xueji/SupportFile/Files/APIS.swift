2290073ef92b7aba08061e7a4ac497d74c1f354d
lvxin <18730231789@163.com>
2017-12-25 10:19:53 +0800

    api文档编辑

--- xueji/SupportFile/Files/APIS.swift
+++ xueji/SupportFile/Files/APIS.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+let base_api = "http://47.52.101.29/app1"
+
+//---------------------------------category
+
+/// 分类列表
+let category_list_api = "/category/list?"
+/// 添加分类
+let category_add_api = "/category/add?"
+/// 插入&更新书籍
+let category_upd_book_api = "/category/upd_book?"
+
+//---------------------------------book
+/// isbn添加
+let book_add_book_api = "/book/add_book?"
+
+/// 自定义添加
+let book_add_custom_api = "/book/add_custom?"
+/// 详情
+let book_detail_api = "/book/detail?"
